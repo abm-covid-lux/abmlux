d87b4340611d6d6daad82bf9610cca894b730711
Steve Wattam <steve@stephenwattam.com>
2020-06-10 11:37:28 +0100

    density model builder now uses pandas for more robust data loading; moved most magic numbers into explicit constants; progress bar

--- density_model.py
+++ density_model.py
@@ -5,34 +5,53 @@
 #This file generates a population density matrix using data collected by the GEOSTAT initiative. Note
 #that Luxembourg measures 57 km x 82 km.
 
-import numpy as np
-from openpyxl import load_workbook
-from tqdm import tqdm
 import math
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
 
 # Config
 INPUT_FILENAME = 'Data/JRC.xlsx'
 OUTPUT_FILENAME = 'Density_Map/Density_Map.csv'
+COORD_OFFSET = (4014, 2934)
 LUXEMBOURG_SIZE_KM = (57, 82)   # width, height
+LUXEMBOURG_COUNTRY_CODE = "LU"
+
+def parse_coordinates(coord_string, x_offset=0, y_offset=0):
+    """Parses a location (what format?),
+
+    returning X, Y coordinates within the space given.
+
+    e.g. 1kmN1621E6359"""
+
+    x = int(coord_string[9:13]) - x_offset
+    y = int(coord_string[4:8]) - y_offset
+
+    return x, y
+
 
 # Load workbook
 print(f"Loading input data from {INPUT_FILENAME}...")
-wgtworkbook = load_workbook(filename=INPUT_FILENAME)
-wgtsheet = wgtworkbook.active
+#wgtworkbook = load_workbook(filename=INPUT_FILENAME)
+#wgtsheet = wgtworkbook.active
+jrc = pd.read_excel(INPUT_FILENAME)
 
-# What are these parameters?
-#xmin = 4014, xmax = 4070, xdifference = 56, ymin = 2934, ymax = 3015, ydifference = 81
+# Filter luxembourg-only rows
+jrc = jrc[jrc["CNTR_CODE"] == LUXEMBOURG_COUNTRY_CODE]
 
-D = [[0 for x in range(LUXEMBOURG_SIZE_KM[0])] for y in range(LUXEMBOURG_SIZE_KM[1])]
+# Iterate over each 1kmx1km location in the input data, reading
+# the location and population density for each.
+density = [[0 for x in range(LUXEMBOURG_SIZE_KM[0])]
+                for y in range(LUXEMBOURG_SIZE_KM[1])]
+for i, row in tqdm(jrc.iterrows(), total=jrc.shape[0]):
 
-# TODO: Where are the numbers 2, 81809 coming from?
-print(f"Calculating Density")
-for s in tqdm(range(2,81809)):
-    if (wgtsheet.cell(row=s, column=3).value == "LU"):
-        x = int(wgtsheet.cell(row=s, column=2).value[9:13]) - 4014  # TODO: Why 4014?
-        y = int(wgtsheet.cell(row=s, column=2).value[4:8]) - 2934   # TODO: Why 2934?
-        D[y][x] = int(wgtsheet.cell(row=s, column=1).value)
+    # Read total population for this 1km chunk, \propto density
+    location_density = row["TOT_P"]
+    x, y             = parse_coordinates(row["GRD_ID"], COORD_OFFSET[0], COORD_OFFSET[1])
+
+    density[y][x] = location_density
 
 print(f"Saving output to {OUTPUT_FILENAME}...")
-np.savetxt(OUTPUT_FILENAME, D, fmt='%i', delimiter=',')
+np.savetxt(OUTPUT_FILENAME, density, fmt='%i', delimiter=',')
 print('Done.') 
