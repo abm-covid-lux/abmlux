afa8728f731ac71f9aa295180378097b32756c81
James-Thompson-724 <66617085+James-Thompson-724@users.noreply.github.com>
2020-07-08 17:47:00 +0200

    Update_2

--- tests/test_distribution_interpolate.py
+++ tests/test_distribution_interpolate.py
@@ -1,4 +1,4 @@
-
+from pytest import approx
 
 class TestDistributionInterpolate:
     """Tests the DistributionInterpolate function in abmlux.density_model"""
@@ -6,18 +6,13 @@ class TestDistributionInterpolate:
     def test_distribution_interpolate():
         """Ensure the total popultion sum is invariant for a test distribution"""
     
-        height = np.random.randint(10)
-        width = np.random.randint(10)
+        height = np.random.randint(1,11)
+        width = np.random.randint(1,11)
         
-        res_fact = 2*np.random.randint(10)
+        res_fact = 2*np.random.randint(1,11)
     
         test_distribution = np.random.randint(0, 100, (height,width))
-
-        print(test_distribution)    
-
         distribution_new = distribution_interpolate(test_distribution,res_fact)
 
-        print(distribution_new)
-
-        # Print the two popultion sums, which upto floating point error should be equal
-        print(np.sum(test_distribution),np.sum(distribution_new))
+        # Assert that the two popultion sums are equal, upto floating point error
+        assert(np.sum(test_distribution)-np.sum(distribution_new)==approx(0))
