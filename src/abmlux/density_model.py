3a6d01e59c76e8cce3bd626621cbe7a5b04804fd
James-Thompson-724 <66617085+James-Thompson-724@users.noreply.github.com>
2020-07-08 16:26:25 +0200

    Update density_model.py

--- src/abmlux/density_model.py
+++ src/abmlux/density_model.py
@@ -59,4 +59,66 @@ def read_density_model_jrc(filepath, country_code):
     # Return the density
     return density
 
+def distribution_interpolate(distribution,res_fact):
 
+    #TODO: ref_fact MUST BE AN EVEN INTEGER
+
+    height = distribution.shape[0]
+    width = distribution.shape[1]
+    
+    #pad with a border of zeros
+    
+    padded_height = height + 2
+    padded_width = width + 2
+    
+    padded_distribution = np.zeros((padded_height,padded_width))
+    padded_distribution[1:height+1,1:width+1] = distribution
+
+    #map padded_density onto a grid within the unit square
+
+    x = np.linspace(0, 1, num=padded_width, endpoint=True)
+    y = np.linspace(0, 1, num=padded_height, endpoint=True)
+    z = padded_distribution
+
+    #linearly interpolate
+
+    f = interp2d(x, y, z)
+
+    #the resolution of the grid is increased by a factor res_fact, and interpolated vaules are assigned to each new square
+
+    x_indent = 1/((padded_width-1)*res_fact*2)
+    y_indent = 1/((padded_height-1)*res_fact*2)
+
+    x_new = np.linspace(x_indent, 1-x_indent, num=(padded_width-1)*res_fact, endpoint=True)
+    y_new = np.linspace(y_indent, 1-y_indent, num=(padded_height-1)*res_fact, endpoint=True)
+        
+    distribution_new = f(x_new,y_new)[int(res_fact/2):len(y_new)-int(res_fact/2),int(res_fact/2):len(x_new)-int(res_fact/2)]
+    
+    #blocks of new squares are normalized to contain equal populations as the original squares
+    
+    for i in range(width):
+        for j in range(height):
+        
+            square = distribution_new[j*res_fact:(j+1)*res_fact, i*res_fact:(i+1)*res_fact]
+        
+            newsum = np.sum(square)
+            oldsum = distribution[j][i]
+            
+            square *= oldsum/newsum
+            
+    return distribution_new
+
+def interpolation_test():
+    
+    height = np.random.randint(10)
+    width = np.random.randint(10)
+    
+    test_distribution = np.random.randint(0, 100, (5,6))
+
+    print(test_distribution)
+
+    distribution_new = distribution_interpolate(test_distribution,2)
+
+    print(distribution_new)
+
+    print(np.sum(test_distribution),np.sum(distribution_new))
