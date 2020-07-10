6ecbaede2f37d72a15dfa000e1037373e2d13378
Steve Wattam <steve@stephenwattam.com>
2020-07-10 14:43:13 +0100

    Updated deps

--- setup.py
+++ setup.py
@@ -94,7 +94,8 @@ setup(
                       'pyYAML',
                       'psutil',
                       'scipy',
-                      'dateparser'],  # Optional
+                      'dateparser',
+                      'matplotlib'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
