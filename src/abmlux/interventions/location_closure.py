26083f79bf7eefb05515a5b913f0544b7fadbde1
James-Thompson-724 <66617085+James-Thompson-724@users.noreply.github.com>
2021-02-04 15:54:50 +0100

    Updated config and care home closure
    Updated care home closure, now access is still permited if an agent works there (this is important actually)

--- src/abmlux/interventions/location_closure.py
+++ src/abmlux/interventions/location_closure.py
@@ -41,3 +41,40 @@ class LocationClosures(Intervention):
             if new_location != home_location:
                 self.bus.publish("request.agent.location", agent, home_location)
                 return MessageBus.CONSUME
+
+class CareHomeClosures(Intervention):
+    """Close a given set of locations.
+
+    In response to a request to change location, this will consume the event and re-publish
+    a request to change location to move home instead."""
+
+    def __init__(self, config, init_enabled):
+        super().__init__(config, init_enabled)
+
+        self.location_closures  = config['locations']
+        self.work_activity_type = None
+        self.home_activity_type = None
+
+    def init_sim(self, sim):
+        super().init_sim(sim)
+
+        self.activity_manager = sim.activity_manager
+
+        self.home_activity_type = sim.activity_manager.as_int(self.config['home_activity_type'])
+        self.work_activity_type = sim.activity_manager.as_int(self.config['work_activity_type'])
+
+        self.bus.subscribe("request.agent.location", self.handle_location_change, self)
+
+    def handle_location_change(self, agent, new_location):
+        """If the new location is in the blacklist, send the agent home."""
+
+        # If disabled, don't intervene
+        if not self.enabled:
+            return
+
+        if new_location.typ in self.location_closures:
+            home_location = agent.locations_for_activity(self.home_activity_type)[0]
+            work_location = agent.locations_for_activity(self.work_activity_type)[0]
+            if new_location != home_location and new_location != work_location:
+                self.bus.publish("request.agent.location", agent, home_location)
+                return MessageBus.CONSUME
